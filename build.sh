46a6ab3d5aeb5f842ad582f59d963e7761205732
SShekotihin <sshekotihindev@gmail.com>
2025-08-12 18:30:52 +0300

    fix: typos

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build --platform linux/arm64,linux/amd64 -f Dockerfile.inference.cpu -t seshhekotikhin/onnxruntime:inference-cpu .
+docker build --platform linux/amd64 -f Dockerfile.inference.cuda -t seshhekotikhin/onnxruntime:inference-cuda .
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker push seshhekotikhin/onnxruntime:inference-cpu
+docker push seshhekotikhin/onnxruntime:inference-cuda
